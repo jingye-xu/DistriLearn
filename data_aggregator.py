b38b3e47e94e85f222f73e38f54c53790808cae4
Gabriel-Morales <47123586+Gabriel-Morales@users.noreply.github.com>
2022-06-13 15:18:25 -0500

    Now also drops garbage data

--- data_aggregator.py
+++ data_aggregator.py
@@ -4,8 +4,10 @@ import os
 import sys
 import pandas as pd
 
+pd.options.mode.use_inf_as_na = True
+
 files = os.listdir()
-files = list(filter(lambda x : x.endswith('.csv'), files))
+files = list(filter(lambda x : x.endswith('.csv') and not x.startswith('aggregate'), files))
 
 data = pd.read_csv(files[0])
 
@@ -19,7 +21,6 @@ cols = list(map(lambda x : x.lstrip(' '), cols))
 cols = list(map(lambda x : x.rstrip('\t'), cols))
 cols = list(map(lambda x : x.rstrip(' '), cols))
 
-
 cols_drops = ['Down/Up Ratio', 'Average Packet Size', 'Avg Fwd Segment Size', 'Avg Bwd Segment Size', 'Fwd Header Length', 'Fwd Avg Bytes/Bulk', 'Fwd Avg Packets/Bulk', 'Fwd Avg Bulk Rate', 'Bwd Avg Bytes/Bulk', 'Bwd Avg Packets/Bulk', 'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', 'Subflow Fwd Bytes', 'Subflow Bwd Packets', 'Subflow Bwd Bytes', 'Init_Win_bytes_forward', 'Init_Win_bytes_backward', 'act_data_pkt_fwd', 'min_seg_size_forward', 'Active Mean', 'Active Std', 'Fwd IAT Total', 'Active Max', 'Active Min', 'Idle Mean', 'Idle Std', 'Idle Max', 'Idle Min', 'Bwd IAT Total', 'Fwd Header Length', 'Bwd Header Length', 'Packet Length Variance']
 
 for name in cols_drops:
@@ -32,6 +33,8 @@ for name in cols_drops:
 
 data.columns = cols
 
+
+data.dropna(inplace=True)
 
 data.drop(cols_drops, axis=1, inplace=True)
 data.to_csv('aggregate_total_data.csv', index=False)
