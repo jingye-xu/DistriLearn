62ae483883baccc20fa498b0b116130ff4a471b0
Gabriel-Morales <47123586+Gabriel-Morales@users.noreply.github.com>
2022-06-15 22:30:11 -0500

    Added scikit flower client

---
+++ scikit_client.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+from collections import OrderedDict
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+
+import joblib
+import warnings
+import flwr as fl
+import pandas as pd
+import os
+import sys
+import numpy as np
+
+def load_data(path=None, filename=None):
+	
+	if path is None:
+		path = "./"
+	if filename is None:
+		filename = "package_result_processed"
+	if not os.path.exists(path + filename + ".csv"):
+		print("file does not exist")
+		sys.exit(1)
+
+	pd.options.mode.use_inf_as_na = True
+	data_dataframe = pd.read_csv(path + filename + ".csv")
+	data_dataframe.dropna(inplace=True)
+
+	# we will let 0 represent benign data
+	# we will let 1 represent malicious data
+
+	data_dataframe.loc[data_dataframe['Label'] == 'BENIGN', 'Label'] = 0.0
+	data_dataframe.loc[data_dataframe['Label'] == 'MALICIOUS', 'Label'] = 1.0
+
+	X = data_dataframe.iloc[:, 0:-1] # Features
+	Y = data_dataframe.iloc[:, -1] # Labels
+	
+	# Convert all data to float type
+	X = X.astype("float32")
+	Y = Y.astype("float32")
+
+	# 20 % Test set size.
+	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3) 
+
+	num_examples = {"trainset": len(X_train), "testset": len(X_test)}
+
+	return X_train, X_test, Y_train, Y_test, num_examples
+
+
+def train(model, X_train, Y_train):
+
+	model.fit(X_train, Y_train)
+
+
+
+def test(model, X_test, Y_test):
+	
+	predictions = model.predict(X_test)
+	accuracy = metrics.accuracy_score(Y_test, predictions) 
+	loss = metrics.log_loss(Y_test, predictions)
+	print(f"Testing accuracy: { accuracy * 100.0 }% ")
+
+	return loss, accuracy
+
+
+# https://github.com/adap/flower/blob/main/examples/sklearn-logreg-mnist/utils.py
+def set_initial_params(model):
+	n_classes = 2
+	n_features = 46  # Number of features in dataset
+	model.classes_ = np.array([i for i in range(n_classes)])
+
+	model.coef_ = np.zeros((n_classes, n_features))
+	if model.fit_intercept:
+		model.intercept_ = np.zeros((n_classes,))
+
+
+
+def main():
+	"""Create model, load data, define Flower client, start Flower client."""
+
+	model = LogisticRegression()
+	set_initial_params(model)
+
+	# Load data 
+	X_train, X_test, Y_train, Y_test, num_examples = load_data(filename='aggregate_total_data')
+
+	# Flower client
+	class ClientTrainer(fl.client.NumPyClient):
+		def get_parameters(self):
+			# return local model parameters
+			# hyperparams = inspect.signature(model.__init__)
+			# return hyperparams
+			return model.get_params()
+
+
+		def set_parameters(self, parameters):
+			for parameter in parameters:
+				st_param = str(parameter)
+				val = model.get_params()[st_param]
+				model.set_params(**{st_param: val})
+			
+			return model
+
+		def fit(self, parameters, config):
+
+			"""
+			Defines the steps to train the model on the locally held dataset. 
+			It also receives global model parameters and other configuration information from the server.
+			"""
+
+			self.set_parameters(parameters)
+			train(model, X_train, Y_train)
+			
+			# save model
+			modelPath = "./svm.pkl"
+			joblib.dump(model, modelPath)
+			
+
+			print("model saved here")
+			return self.get_parameters(), num_examples["trainset"], {}
+
+
+		def evaluate(self, parameters, config):
+			# evaluating the provided parameters using a locally held dataset
+			self.set_parameters(parameters)
+			loss, accuracy = test(model, X_test, Y_test)
+			return float(loss), num_examples["testset"], {"accuracy": float(accuracy)}
+
+
+	# Start client
+	fl.client.start_numpy_client("127.0.0.1:8080", client=ClientTrainer())
+
+
+if __name__ == "__main__":
+	main()
